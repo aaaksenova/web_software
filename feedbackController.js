53532fa155d9cc73e62e58c8cfe649d40190dc09
aksenovaanna <aaaksenova2@gmail.com>
2024-10-07 15:23:01 +0300

    add cookies

--- feedbackController.js
+++ feedbackController.js
@@ -1,6 +1,7 @@
 import * as feedbacks from "./feedbackService.js";
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+import { getSignedCookie, setSignedCookie } from "https://deno.land/x/hono@v3.12.11/helper.ts";
 
 // Define Zod schema for course name validation
 const courseNameSchema = z.string().min(4, {
@@ -8,6 +9,7 @@ const courseNameSchema = z.string().min(4, {
 });
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+const secret = "secret";
 
 
 const getCourseFeedback = async (c) => {
@@ -22,12 +24,26 @@ const getCourseFeedback = async (c) => {
 const postCourseFeedback = async (c) => {
   const courseId = c.req.param("courseId");
   const feedbackType = c.req.param("feedbackType");
+  const userId = await getSignedCookie(c, secret, "userId") ?? crypto.randomUUID();
+  await setSignedCookie(c, "userId", userId, secret, { path: "/" });
+
+  // Get the courses the user has already provided feedback for from the cookie
+  let feedbackGivenCourses = JSON.parse(await getSignedCookie(c, secret, "feedbackGivenCourses") || "[]");
+
+  if (feedbackGivenCourses.includes(courseId)) {
+    return c.redirect(`/courses/${courseId}?alreadyGiven=true`);
+  }
 
   // Increment feedback count for the specific course and feedback type
   await feedbacks.incrementFeedbackCount(courseId, feedbackType);
 
+  // Update the cookie to include the current courseId
+  feedbackGivenCourses.push(courseId);
+  await setSignedCookie(c, "feedbackGivenCourses", JSON.stringify(feedbackGivenCourses), secret, { path: "/" });
+
   return c.redirect(`/courses/${courseId}`);
 };
+
 
 const showFeedbackForm = async (c) => {
   const html = await eta.render("index.eta");
@@ -68,10 +84,17 @@ const showCourse = async (c) => {
     for (let i = 1; i <= 5; i++) {
       feedbackCounts[i] = await feedbacks.getFeedbackCount(courseId, i.toString());
     }
-    // course = course.value
-    // console.log(course)
+
+    const userId = await getSignedCookie(c, secret, "userId") ?? crypto.randomUUID();
+    const feedbackGivenCourses = JSON.parse(await getSignedCookie(c, secret, "feedbackGivenCourses") || "[]");
   
-    const html = await eta.render("course.eta", { course,  feedbackCounts});
+    const hasGivenFeedback = feedbackGivenCourses.includes(courseId);
+  
+    const html = await eta.render("course.eta", 
+                                    { course,  
+                                    feedbackCounts,
+                                    hasGivenFeedback});
+
     return c.html(html);
   };
   
